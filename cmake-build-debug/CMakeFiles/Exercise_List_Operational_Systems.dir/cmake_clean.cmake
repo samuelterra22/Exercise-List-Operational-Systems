d4d38cfafe14fbc929fd826cf6e48c13e38c5e21
Samuel Terra <samuelterra22@hotmail.com>
2019-08-27 19:46:14 -0300

    Add cmake files

---
+++ cmake-build-debug/CMakeFiles/Exercise_List_Operational_Systems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_List_Operational_Systems.dir/List_01/exercise_02.c.o"
+  "Exercise_List_Operational_Systems.pdb"
+  "Exercise_List_Operational_Systems"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise_List_Operational_Systems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
