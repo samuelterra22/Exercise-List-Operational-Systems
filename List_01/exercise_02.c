c2d08a2f6ae5fcdcad5ae967486378b7d2207103
Samuel Terra <samuelterra22@hotmail.com>
2019-08-27 18:08:21 -0300

    Add order

---
+++ List_01/exercise_02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ Fazer um programa que receba uma lista de nomes de usuário de um SO juntamente com seu
+identificador uid. Uma lista encadeada ordernada por uid deve ser criada para guardar essas
+informações. Após isso, uma lista de processos juntamente com o id do usuário que o criou é entrada
+pelo usuário. Dentro de cada nó da lista de nomes de usuário, deve ser armazenado o ponteiro de início
+de uma outra lista, a lista de processos do respectivo usuário. No final deve ser apresentado cada
+usuário, seu id e a lista de processos que estão executando com seu uid.
+*/
+
+int main(void) {
+    printf("Exercício 02");
+
+    exit(EXIT_SUCCESS);
+}
