8634d7fe8716cf23fee88273922d7052c7f0b373
Samuel Terra <samuelterra22@hotmail.com>
2019-08-27 19:37:23 -0300

    Create insert list

--- List_01/exercise_02.c
+++ List_01/exercise_02.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
  Fazer um programa que receba uma lista de nomes de usuário de um SO juntamente com seu
@@ -10,8 +11,102 @@ de uma outra lista, a lista de processos do respectivo usuário. No final deve s
 usuário, seu id e a lista de processos que estão executando com seu uid.
 */
 
+typedef struct tProcessList {
+    int userId;
+    char *name;
+} ProcessList;
+
+typedef struct tUserList {
+    int uid;
+    char *name;
+    struct tUserList *next;
+    ProcessList *processList;
+} UserList;
+
+void insert(UserList **p, int uid, char *name) {
+    UserList *current, *new, *previous;
+
+    new = (UserList *) malloc(sizeof(UserList));
+
+    current = *p;
+    previous = NULL;
+
+    new->uid = uid;
+    new->name = malloc(strlen(name));
+    strcpy(new->name, name);
+
+    // ProcessList *pl = (ProcessList *) malloc(sizeof(ProcessList));
+    // new->processList = pl;
+
+    if (current == NULL) {
+        new->next = NULL;
+        *p = new;
+    } else {
+        while (current != NULL && current->uid < uid) {
+            previous = current;
+            current = current->next;
+        }
+
+        new->next = current;
+
+        if (previous == NULL) {
+            *p = new;
+        } else {
+            previous->next = new;
+        }
+    }
+}
+
+void print_list(UserList *p) {
+    while (p != NULL) {
+        printf("-------------------------------\n");
+        printf("uid: %d\n", p->uid);
+        printf("name: %s\n", p->name);
+        printf("---------- Processos ----------\n");
+        while (p->processList != NULL) {
+            printf("%s\n", p->processList->name);
+        }
+        printf("-------------------------------\n");
+        p = p->next;
+    }
+}
+
+void *destroy_list(UserList *l) {
+    UserList *p = l;
+    while (p != NULL) {
+        UserList *t = p->next;
+        free(p->name);
+        free(p);
+        p = t;
+    }
+    return NULL;
+}
+
+
 int main(void) {
-    printf("Exercício 02");
+    UserList *list = NULL;
+    int quantity = 0;
+    int uid;
+    char *name;
+
+    printf("Insira o número de entradas: ");
+    scanf("%d", &quantity);
+
+    printf("-------------------------------\n");
+    for (int i = 0; i < quantity; i++) {
+        printf("Insira o nome do usuário[%d]: ", i);
+        scanf("%s", name);
+
+        printf("Insira o uid do usuário[%d]: ", i);
+        scanf("%d", &uid);
+
+        insert(&list, uid, name);
+        printf("-------------------------------\n");
+    }
+
+    print_list(list);
+    list = destroy_list(list);
+    print_list(list);
 
     exit(EXIT_SUCCESS);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Exercise-List-Operational-Systems
+# Exercise-UserList-Operational-Systems
