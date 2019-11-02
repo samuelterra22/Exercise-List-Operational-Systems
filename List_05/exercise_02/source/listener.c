cef185625981a85953a22f274cbed1170b354068
Samuel Terra <samuelterra22@hotmail.com>
2019-11-02 00:11:03 -0300

    Improve listener

--- List_05/exercise_02/source/listener.c
+++ List_05/exercise_02/source/listener.c
@@ -22,90 +22,121 @@
  *****************************************************************************/
 
 void handle_message(struct Message *message, char *watcher_path, char *listener_path) {
-    // IN_CREATE
-    // IN_DELETE
-    // IN_MODIFY
-    // IN_MOVED_FROM
-    // IN_MOVED_TO
+	// IN_CREATE
+	// IN_DELETE
+	// IN_MODIFY
+	// IN_MOVED_FROM
+	// IN_MOVED_TO
 
-    char *path_tmp = malloc(sizeof(char) * strlen(listener_path));
-    strcpy(path_tmp, listener_path);
+	char *path_tmp = malloc(sizeof(char) * strlen(listener_path));
+	strcpy(path_tmp, listener_path);
 
-    struct inotify_event *event = extract_item(message);
+	struct inotify_event *event = extract_item(message);
 
-    display_inotify_event(event);
+	display_inotify_event(event);
 
-    if (event->mask & IN_CREATE){
-        if (event->mask & IN_ISDIR){
-            mkdir(strcat(path_tmp, event->name), 0644);
-        } else {
-            int fd = open(strcat(path_tmp, event->name), O_CREAT);
-            close(fd);
-        }
-    }
+	if (event->mask & IN_CREATE) {
+		if (event->mask & IN_ISDIR) {
+			mkdir(strcat(path_tmp, event->name), 0644);
+		} else {
+			int fd = open(strcat(path_tmp, event->name), O_CREAT);
+			close(fd);
+		}
+	}
 
-    if (event->mask & IN_DELETE){
-        if (event->mask & IN_ISDIR){
-            rmdir(strcat(path_tmp, event->name));
-        } else {
-            remove(strcat(path_tmp, event->name));
-        }
-    }
+	if (event->mask & IN_DELETE) {
+		if (event->mask & IN_ISDIR) {
+			rmdir(strcat(path_tmp, event->name));
+		} else {
+			remove(strcat(path_tmp, event->name));
+		}
+	}
 
-    if (event->mask & IN_MODIFY){
-        printf("path: %s", watcher_path);
-        // remove(event->name);
-    }
+	if (event->mask & IN_MODIFY) {
+		printf("----------- move from %s\n", watcher_path);
+		printf("----------- move to %s\n", listener_path);
+
+		char source[PATH_MAX], destiny[PATH_MAX];
+		FILE *source_file, *destiny_file;
+
+		strcpy(source, watcher_path);
+		strcpy(destiny, listener_path);
+
+		strcat(source, event->name);
+		strcat(destiny, event->name);
+
+		source_file = fopen(source, "r");
+		destiny_file = fopen(destiny, "w");
+
+		if (source_file == NULL) {
+			printf("Cannot open file %s \n", source);
+			exit(0);
+		}
+		if (destiny_file == NULL) {
+			printf("Cannot open file %s \n", destiny);
+			exit(0);
+		}
+
+		char c = fgetc(source_file);
+		while (c != EOF) {
+			fputc(c, destiny_file);
+			c = fgetc(source_file);
+		}
+
+
+		fclose(source_file);
+		fclose(destiny_file);
+	}
 }
 
 int main(int argc, char *argv[]) {
-    setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 
-    mqd_t watcher;
-    struct mq_attr mq_attr;
-    struct Message m;
+	mqd_t watcher;
+	struct mq_attr mq_attr;
+	struct Message m;
 
-    struct mq_attr attr;
-    attr.mq_flags = 0;                          /* Flags: 0 or O_NONBLOCK */
-    attr.mq_maxmsg = 10;                        /* Max. # of messages on queue */
-    attr.mq_msgsize = sizeof(struct Message) + NAME_MAX + 1;   /* Max. Message size (bytes) */
-    attr.mq_curmsgs = 0;                        /* # of messages currently in queue */
+	struct mq_attr attr;
+	attr.mq_flags = 0;                          /* Flags: 0 or O_NONBLOCK */
+	attr.mq_maxmsg = 10;                        /* Max. # of messages on queue */
+	attr.mq_msgsize = sizeof(struct Message) + NAME_MAX + 1;   /* Max. Message size (bytes) */
+	attr.mq_curmsgs = 0;                        /* # of messages currently in queue */
 
-    if (argv[1] == NULL) {
-        printf("Inform the watcher path!\n");
-        printf("example to run: $ ./listener [watcher_path] [listener_path]\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argv[1] == NULL) {
+		printf("Inform the watcher path!\n");
+		printf("example to run: $ ./listener [watcher_path] [listener_path]\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if (argv[2] == NULL) {
-        printf("Inform the listener path!\n");
-        printf("example to run: $ ./listener [watcher_path] [listener_path]\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argv[2] == NULL) {
+		printf("Inform the listener path!\n");
+		printf("example to run: $ ./listener [watcher_path] [listener_path]\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc < 2 || strcmp(argv[1], "--help") == 0)
-        printf("%s pathname...\n", argv[0]);
+	if (argc < 2 || strcmp(argv[1], "--help") == 0)
+		printf("%s pathname...\n", argv[0]);
 
-    watcher = mq_open(MQ_NAME_WATCHER, O_RDWR | O_CREAT, 0644, &attr);
+	watcher = mq_open(MQ_NAME_WATCHER, O_RDWR | O_CREAT, 0644, &attr);
 
-    if (watcher == -1) {
-        perror("can not create watcher msg queue\n");
-        exit(1);
-    }
+	if (watcher == -1) {
+		perror("can not create watcher msg queue\n");
+		exit(1);
+	}
 
-    mq_getattr(watcher, &mq_attr);
-    printf("mq maximum msgsize = %d\n", (int) mq_attr.mq_msgsize);
+	mq_getattr(watcher, &mq_attr);
+	printf("mq maximum msgsize = %d\n", (int) mq_attr.mq_msgsize);
 
-    while (TRUE) {
-        receive(watcher, &m);
+	while (TRUE) {
+		receive(watcher, &m);
 
 //        display_inotify_event(item);
 
-        handle_message(&m, argv[1], argv[2]);
-    }
+		handle_message(&m, argv[1], argv[2]);
+	}
 
-    mq_close(watcher);
-    return EXIT_SUCCESS;
+	mq_close(watcher);
+	return EXIT_SUCCESS;
 }
 
 #pragma clang diagnostic pop
